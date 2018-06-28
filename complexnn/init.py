ac264a48164cb3d4f3c33771721bdffeefbd8b39
Parcollet Titouan <titouan.parcollet@alumni.univ-avignon.fr>
2018-06-28 17:49:33 +0200

    Update init.py
    Adding qdense_init

--- complexnn/init.py
+++ complexnn/init.py
@@ -92,7 +92,67 @@ class QuaternionInit(Initializer):
 
 		return weight
 
+class qdense_init(Initializer):
+	# The standard complex initialization using
+	# either the He or the Glorot criterion.
+	def __init__(self, shape, criterion='he', seed=None):
 
+		# `weight_dim` is used as a parameter for sanity check
+		# as we should not pass an integer as kernel_size when
+		# the weight dimension is >= 2.
+		# nb_filters == 0 if weights are not convolutional (matrix instead of filters)
+		# then in such a case, weight_dim = 2.
+		# (in case of 2D input):
+		#     nb_filters == None and len(kernel_size) == 2 and_weight_dim == 2
+		# conv1D: len(kernel_size) == 1 and weight_dim == 1
+		# conv2D: len(kernel_size) == 2 and weight_dim == 2
+		# conv3d: len(kernel_size) == 3 and weight_dim == 3
+
+		self.shape = shape
+		self.criterion = criterion
+		self.seed = 1337 if seed is None else seed
+
+	def __call__(self, shape, dtype=None):
+
+		fan_in  = self.shape[0]
+		fan_out = self.shape[1]
+
+		# Quaternion operations start here
+
+		if self.criterion == 'glorot':
+			s = 1. / np.sqrt(2*(fan_in + fan_out))
+		elif self.criterion == 'he':
+			s = 1. / np.sqrt(2*fan_in)
+		else:
+			raise ValueError('Invalid criterion: ' + self.criterion)
+
+		#Generating randoms and purely imaginary quaternions :
+		number_of_weights = np.prod(self.shape) 
+		v_i = np.random.uniform(0.0,1.0,number_of_weights)
+		v_j = np.random.uniform(0.0,1.0,number_of_weights)
+		v_k = np.random.uniform(0.0,1.0,number_of_weights)
+		#Make these purely imaginary quaternions unitary
+		for i in range(0, number_of_weights):
+			norm = np.sqrt(v_i[i]**2 + v_j[i]**2 + v_k[i]**2)+0.0001
+			v_i[i]/= norm
+			v_j[i]/= norm
+			v_k[i]/= norm
+		v_i = v_i.reshape(self.shape)
+		v_j = v_j.reshape(self.shape)
+		v_k = v_k.reshape(self.shape)
+
+		rng = RandomState(self.seed)
+		modulus = rng.rayleigh(scale=s, size=self.shape)
+		phase = rng.uniform(low=-np.pi, high=np.pi, size=self.shape)
+		
+		weight_r = modulus * np.cos(phase)
+		weight_i = modulus * v_i*np.sin(phase)
+		weight_j = modulus * v_j*np.sin(phase)
+		weight_k = modulus * v_k*np.sin(phase)
+
+		weight = np.concatenate([weight_r, weight_i, weight_j, weight_k], axis=-1)
+		
+		return weight
 
 class SqrtInit(Initializer):
 	def __call__(self, shape, dtype=None):
